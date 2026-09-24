04aee0d2d05074b1482d5c95cf22db13ee58413e
luisjulianfried-stack <luis.julian.fried@gmail.com>
2026-09-24 13:35:36 +0200

    Use balloon LL in system mark

--- components/system-mark.tsx
+++ components/system-mark.tsx
@@ -2,11 +2,11 @@ export function SystemMark({ active = 0 }: { active?: number }) {
   return (
     <img
       key={active}
-      src="/brand/ll-monogram.webp"
+      src="/brand/ll-balloon.svg"
       alt="LL Collective Studio Monogram"
       className="system-mark"
-      width={1254}
-      height={1254}
+      width={512}
+      height={512}
       style={{ display: 'block', width: '100%', height: 'auto', objectFit: 'contain' }}
     />
   );
